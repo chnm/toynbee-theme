005620b9b63824bb0a6866d24afa5b6631ed6daa
Kim Nguyen <design@kimisgold.com>
2012-04-26 00:49:48 -0400

    Fixed border-image rendering in Safari.

--- ngh-people.php
+++ ngh-people.php
@@ -25,12 +25,12 @@
 				
 				<?php 
 				
-				    $taggedUsers = get_users('meta_value=ngh-blog');
-				    foreach ($taggedUsers as $user) {
-				        $currentUser = $user->ID;
+				    $tagged_users = get_users('meta_value=ngh-blog');
+				    foreach ($tagged_users as $user) {
+				        $current_user = $user->ID;
 				        echo '<p>';
-				        echo '<span class="member-name">' . get_user_meta($currentUser,'first_name', true) . ' ' . get_user_meta($currentUser,'last_name', true) . '</span>' ;
-				        echo '<br><span class="member-description">' . get_user_meta($currentUser,'description', true) . '</span>';
+				        echo '<span class="member-name">' . get_user_meta($current_user,'first_name', true) . ' ' . get_user_meta($current_user,'last_name', true) . '</span>' ;
+				        echo '<br><span class="member-description">' . get_user_meta($current_user,'description', true) . '</span>';
 				        echo '</p>';
 				        }
 				    
